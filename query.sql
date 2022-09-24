7c02ec87241762cfffee9e49e01b2c78a3bb51d2
Daniel Garcia <danielgarciaabello@gmail.com>
2022-09-24 11:12:14 -0500

    query sql

--- query.sql
+++ query.sql
@@ -1,56 +1,53 @@
-create table Persona(
-   id_persona int not null auto_increment,
+create table pais(
+   id_pais serial,
+   nombre varchar(30) not null,       
+   PRIMARY KEY (id_pais)
+  
+);
+create table departamento(
+   id_departamento serial,
+   pais_id int not null,
+   Nombre varchar(30) not null,       
+   PRIMARY KEY (id_departamento),
+   FOREIGN KEY (pais_id) REFERENCES pais(id_pais)
+);
+
+create table ciudad(
+   id_ciudad serial,
+   departamento_id int not null,
+   nombre varchar(30) not null,       
+   PRIMARY KEY (id_ciudad),
+   FOREIGN KEY (departamento_id) REFERENCES departamento(id_departamento)
+);
+
+
+create table persona(
+   id_persona serial,
    ciudad_id int not null,
    nombre varchar(30) not null, 
    apellido varchar(30) not null,      
    PRIMARY KEY (id_persona),
-   FOREIGN KEY (ciudad_id) REFERENCES Ciudad(id_ciudad)
+   FOREIGN KEY (ciudad_id) REFERENCES ciudad(id_ciudad)
 );
 
-create table Ciudad(
-   id_ciudad int not null auto_increment,
-   departamento_id int not null,
-   nombre varchar(30) not null,       
-   PRIMARY KEY (id_ciudad),
-   FOREIGN KEY (departamento_id) REFERENCES Departamento(id_departamento)
-);
+insert into pais(nombre) values('colombia');
+insert into pais(nombre) values('argentina');
+insert into pais(nombre) values('francia');
+insert into pais(nombre) values('España');
 
-create table Departamento(
-   id_departamento int not null auto_increment,
-   pais_id int not null,
-   Nombre varchar(30) not null,       
-   PRIMARY KEY (id_departamento),
-   FOREIGN KEY (pais_id) REFERENCES Pais(id_pais)
-);
+insert into departamento(pais_id, nombre) values(1, 'tolima');
+insert into departamento(pais_id, nombre) values(2, 'buenos aires');
+insert into departamento(pais_id, nombre) values(3, 'paris');
+insert into departamento(pais_id, nombre) values(4, 'madrid');
 
-create table Pais(
-   id_pais int not null auto_increment,
-   nombre varchar(30) not null,       
-   PRIMARY KEY (id_pais),
-  
-);
-----Paises
-insert into Pais(nombre) values('colombia');
-insert into Pais(nombre) values('argentina');
-insert into Pais(nombre) values('francia');
-insert into Pais(nombre) values('España');
+insert into ciudad(departamento_id, nombre) values (1, 'ibague');
+insert into ciudad(departamento_id, nombre) values (2, 'provincia argentina');
+insert into ciudad(departamento_id, nombre) values (3, 'parque de los principes');
+insert into ciudad(departamento_id, nombre) values (4, 'valdevedas');
 
-----Departamentos
-insert into Departamento(pais_id, nombre) values(1, 'tolima');
-insert into Departamento(pais_id, nombre) values(2, 'buenos aires');
-insert into Departamento(pais_id, nombre) values(3, 'paris');
-insert into Departamento(pais_id, nombre) values(4, 'madrid');
-
------Ciudades
-insert into Ciudad(departamento_id, nombre) values (1, 'ibague');
-insert into Ciudad(departamento_id, nombre) values (2, 'provincia argentina');
-insert into Ciudad(departamento_id, nombre) values (3, 'parque de los principes');
-insert into Ciudad(departamento_id, nombre) values (4, 'valdevedas');
-
-----Personas
-insert into Persona(ciudad_id, nombre, apellido) values(1, 'michael', 'garcia');
-insert into Persona(ciudad_id, nombre, apellido) values(2, 'Elio', 'ortiz');
-insert into Persona(ciudad_id, nombre, apellido) values(3, 'juan', 'castillon');
-insert into Persona(ciudad_id, nombre, apellido) values(4, 'ana', 'molano');
+insert into persona(ciudad_id, nombre, apellido) values(1, 'michael', 'garcia');
+insert into persona(ciudad_id, nombre, apellido) values(2, 'Elio', 'ortiz');
+insert into persona(ciudad_id, nombre, apellido) values(3, 'juan', 'castillon');
+insert into persona(ciudad_id, nombre, apellido) values(4, 'ana', 'molano');
 
 
